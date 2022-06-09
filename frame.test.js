c3f236eaea7cfd13c24e405f8c30f9ad7733bd40
S-Spiegl <steven.spiegl@gmail.com>
2022-06-10 00:40:31 +0100

    frame score implemented

--- frame.test.js
+++ frame.test.js
@@ -17,4 +17,17 @@ describe('Frame', () => {
       expect(frame.rolls.length).toBe(2)
     })
   })
+
+  describe('.score', () => {
+    it('calculates the score for an incomplete frame', () => {
+      frame.addRoll(5);
+      expect(frame.score()).toBe(5)
+    })
+
+    it('calculates the score for a complete frame', () => {
+      frame.addRoll(5),
+      frame.addRoll(3)
+      expect(frame.score()).toBe(8);
+    })
+  })
 })
